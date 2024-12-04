60af9db1102f5583250d690c38ffc05cfd9af344
Andrewkaliszak <andrew.kaliszak@escola.pr.gov.br>
2024-12-04 20:14:12 -0300

    Create movis.js

---
+++ movis.js
@@ -0,0 +1,454 @@
+MOVIES.JS 
+export const movies = [ { 
+title: "Bela e a fera
+", 
+genre: "Ação", 
+ageRating: Livre, 
+synopsis: "Em uma pequena aldeia da França vive Belle, uma jovem inteligente que é considerada estranha pelo moradores da localidade, e seu pai, Maurice, um inventor que é visto como um louco. Ela é cortejada por Gaston, que quer casar com ela. Mas apesar de todas as jovens do lugarejo o acharem um homem bonito, Belle não o suporta, pois vê nele uma pessoa primitiva e convencida. Quando o pai de Belle vai para uma feira demonstrar sua nova invenção, ele acaba se perdendo na floresta e é atacado por lobos. Desesperado, Maurice procura abrigo em um castelo, mas acaba se tornando prisioneiro da Fera, o senhor do castelo, que na verdade é um príncipe que foi amaldiçoado por uma feiticeira quando negou abrigo a ela. Quando Belle sente que algo aconteceu ao seu pai vai à sua procura. Ela chega ao castelo e lá faz um acordo com a Fera: se seu pai fosse libertado ela ficaria no castelo para sempre. A Fera concorda e todos os "moradores" do castelo, que lá vivem e também foram transformados em objetos falantes, sentem que esta pode ser a chance do feitiço ser quebrado. Mas isto só acontecerá se a Fera amar alguém e esta pessoa retribuir o seu amor, sendo que isto tem de ser rápido, pois quando a última pétala de uma rosa encantada cair o feitiço não poderá ser mais desfeito.
+", 
+trailer: "https://www.youtube.com/watch?v=3Qxt86RCuH0
+" 
+}, 
+{ 
+title: " Do outro lado
+", 
+genre: "Ação", 
+ageRating: 10, 
+synopsis: "Um garoto de 9 anos nunca atravessou o outro lado da estrada. Para ele, é como se fosse outro planeta: desperta admiração e medo. Tudo parece mais interessante, mas também muito perigoso. Sua única diversão é observar o outro lado e colecionar coisas que ele sabe que vieram de lá. Mas algo inesperado acontece e muda seu destino para sempre.
+", 
+trailer: "https://youtu.be/ii5YiDcsGiY
+" 
+}, 
+{ 
+title: "Crianças invisíveis
+", 
+genre: "Ação", 
+ageRating: 12, 
+synopsis: "Seja coletando sucata nas ruas de São Paulo ou roubando para viver em Nápoles e no interior da Sérvia, os filmes são protagonizados por personagens infantis que lidam com uma dura realidade, na qual crescer muito cedo acaba sendo a única saída.
+", 
+trailer: "https://www.youtube.com/watch?v=trGs9Jmeom0" 
+}, 
+{ 
+title: "O milagre da sela 7
+", 
+genre: "Ação", 
+ageRating: 14, 
+synopsis: "Memo (Aras Bulut Iynemli), um pastor de ovelhas com deficiência mental, vive com a filha Ova (Nisa Sofiya Aksongur) e com a avó Fatma (Celile Toyon Uysal) em uma vila na costa turca do mar Egeu durante o período do golpe de estado em 80.
+Um belo dia, em 1983, sua vida é subitamente virada de cabeça para baixo quando a filha do comandante, um oficial sênior durante a lei marcial, morre tragicamente.
+Memo é injustamente acusado do assassinato e condenado à morte. Ele acaba na prisão, na cela número sete. Lá, inicialmente recebido com ódio, gradualmente convence os colegas de cela de sua inocência e mostra seu grande coração. Com o tempo, todos ali se mobilizam para tentar salvar sua vida.
+Remake do filme homônimo de Hwan-kyung Lee, lançado em 2013.
+
+", 
+trailer: "https://www.youtube.com/watch?v=Go3OQn1YomI
+" 
+}, 
+{ 
+title: "Amor e Monstros 
+", 
+genre: "Ação", 
+ageRating: 16, 
+synopsis: "Em um futuro apocalíptico invadido por seres geneticamente modificados, a população sobrevive em bunkers e esconderijos há sete anos. Joel Dawson (Dylan O’Brien) não consegue achar sua verdadeira função dentro do grupo em que convive, por isso, decide se arriscar em uma aventura para reencontrar sua ex-namorada da escola. O filme é despretensioso e não tem medo de parecer bobo. Depois de um tempo, você nem percebe que tudo surgiu de um bizarro apocalipse de monstros. Dylan O’Brien sabe rir de si mesmo e gera simpatia e diversão ao dar vida ao protagonista.
+", 
+trailer: "https://www.youtube.com/watch?v=AaUdMfh5j3U
+" 
+}, 
+{ 
+title: "Toy Story
+", 
+genre: "Comédia", 
+ageRating: Livre, 
+synopsis: "O aniversário do garoto Andy está chegando e seus brinquedos ficam nervosos, temendo que ele ganhe novos brinquedos que possam substituí-los. Liderados pelo caubói Woody, o brinquedo predileto de Andy, eles recebem Buzz Lightyear, o boneco de um patrulheiro espacial, que logo passa a receber mais atenção do garoto. Com ciúmes, Woody tenta ensiná-lo uma lição, mas Buzz cai pela janela. É o início da aventura do caubói, que precisa resgatar Buzz para limpar sua barra com os outros brinquedos.
+ ", 
+trailer: "https://www.youtube.com/watch?v=4YKv__f7KzE
+" 
+}, 
+{ 
+title: ":Jumanji
+",
+genre: "Comédia", 
+ageRating: 10, synopsis: "Quatro adolescentes encontram um videogame cuja ação se passa em uma floresta tropical. Empolgados com o jogo, eles escolhem seus avatares para o desafio, mas um evento inesperado faz com que eles sejam transportados para dentro do universo fictício, transformando-os nos personagens da aventura.
+", 
+trailer: "https://www.youtube.com/watch?v=veszTagaXik
+" 
+}, 
+{ 
+title: ":Os Farofeiros 2
+", 
+genre: "Comedia", 
+ageRating: 12, synopsis: "Os colegas de trabalho Alexandre, Lima, Rocha, Diguinho e suas famílias são presenteados pela empresa com uma viagem para a Bahia. No entanto, problemas e imprevistos podem levar por água abaixo essa viagem dos sonhos.", 
+trailer: "https://www.youtube.com/watch?v=dvmR-li9yQE
+" 
+}, 
+{ 
+title: "Os novatos", 
+genre: "Comedia", 
+ageRating: 14, synopsis: "A maior festa do ano vai acontecer bem na primeira semana do ensino médio. Quatro adolescentes encaram uma noite de caos e excessos.
+", 
+trailer: "https://www.youtube.com/watch?v=dkK5G1ARFrM
+" 
+}, 
+{ 
+title: "Todo Mundo em Pânico(paródia)
+", 
+genre: "Comedia", 
+ageRating: 16, synopsis: "Depois do assassinato de uma bela colega de classe, um grupo de adolescentes desorientados descobre que há um assassino entre eles. A heroína Cindy Campbell e a sua turma de amigos tentam se proteger do perigo, mas Gail Hailstorm, uma repórter irritante, simplesmente não os deixa em paz.
+", 
+trailer: "https://www.youtube.com/watch?v=JI0RSYE7Pzk
+" 
+}, 
+{ 
+title: "À Procura da Felicidade
+", 
+genre: "Drama", 
+ageRating: Livre, synopsis: "Em À Procura da Felicidade, Chris Gardner (Will Smith) é um pai de família que enfrenta sérios problemas financeiros. Apesar de todas as tentativas em manter a família unida, Linda (Thandie Newton), sua esposa, decide partir. Chris agora é pai solteiro e precisa cuidar de Christopher (Jaden Smith), seu filho de apenas 5 anos. Ele tenta usar sua habilidade como vendedor para conseguir um emprego melhor, que lhe dê um salário mais digno. Chris consegue uma vaga de estagiário numa importante corretora de ações, mas não recebe salário pelos serviços prestados. Sua esperança é que, ao fim do programa de estágio, ele seja contratado e assim tenha um futuro promissor na empresa. Porém seus problemas financeiros não podem esperar que isto aconteça, o que faz com que sejam despejados. Chris e Christopher passam a dormir em abrigos, estações de trem, banheiros e onde quer que consigam um refúgio à noite, mantendo a esperança de que dias melhores virão.
+", 
+trailer: "https://www.youtube.com/watch?v=RO6knM2cUYQ
+" 
+}, 
+{ 
+title: "Klaus
+", 
+genre: "Drama", 
+ageRating: 10, synopsis: "Um carteiro egoísta e um fabricante de brinquedos solitário cultivam uma amizade improvável e levam alegria a uma cidade fria e sombria.
+", 
+trailer: "https://www.youtube.com/watch?v=ILy2vKcI6fo
+" 
+}, 
+{ 
+title: "Som da Esperança ", 
+genre: "Drama", 
+ageRating: 12, synopsis: "Donna e o Reverendo Martin iniciam um movimento para que famílias adotem crianças do sistema de acolhimento que são difíceis de colocar.
+", 
+trailer: "https://www.youtube.com/watch?v=JGNyqveOyHw
+" 
+},
+{ 
+title: "À Espera de um Milagre
+", 
+genre: "Drama", 
+ageRating: 14, 
+synopsis: "Um carcereiro tem um relacionamento incomum e comovente com um preso que está no corredor na morte: Coffey, um negro enorme, condenado por ter matado brutalmente duas gêmeas de nove anos. Ele tem tamanho e força para matar qualquer um, mas seu comportamento é completamente oposto à sua aparência. Além de ser simples, ingênuo e ter pavor do escuro, ele possui um dom sobrenatural. Com o passar do tempo, o carcereiro aprende que, às vezes, os milagres acontecem nos lugares mais inesperados.
+", 
+trailer: "https://www.youtube.com/watch?v=EIc_wJtm6AU
+" 
+}, 
+{ 
+title: ":A Onda
+", 
+genre: "Drama", 
+ageRating: 16, 
+synopsis: "Rainer é um professor a quem foi designada a tarefa de instruir seus estudantes de Ensino Médio sobre o Estado Autocrático durante uma sessão às lições longas. Um professor favorito entre as crianças, Rainer decide deixar seus alunos desenvolver o assunto e pede a eles que construam sua própria autocracia. No entanto, quando as crianças formam um Estado-nação similar com o da Alemanha nazista, os professores não sabem o que fazer.
+", 
+trailer: "https://www.youtube.com/watch?v=N9vdfb2f-B0
+" 
+}, 
+{ 
+title: "Vida de Inseto
+", 
+genre: "Animação", 
+ageRating: Livre, 
+synopsis: "Flik é uma formiga cheia de ideias e sempre causa problemas na sua colônia. Seu último acidente foi destruir os alimentos que seriam usados para pagamento ao gafanhoto Hopper. Agora, Hopper está exigindo o dobro, senão a colônia será aniquilada. Para evitar o desastre, Flik tenta recrutar combatentes para defender o local, e quando encontra um bando de insetos voadores, ele acredita que encontrou a sua salvação.
+", 
+trailer: "https://www.youtube.com/watch?v=L6CbIRt0wmY
+" 
+}, 
+{ 
+title: "Memórias de Ontem", 
+genre: "Animação", 
+ageRating: 10, 
+synopsis: "Taeko é uma mulher solteira que se dedica apenas ao trabalho. Ela sai de sua nativa Tóquio pela primeira vez e viaja a Yamagata para visitar a família da irmã durante a colheita anual de açafrão-bastardo. Ao longo da viagem, ela começa a questionar se sua vida estressante é o que desejava quando jovem.
+", 
+trailer: "https://www.youtube.com/watch?v=5gSKk-wwLsY
+" 
+}, 
+{ 
+title: "iga da justiça sombria
+", 
+genre: "Animação", 
+ageRating: 14, 
+synopsis: "A Liga da Justiça encontra uma força sobrenatural contra a qual seus poderes parecem ser inúteis, e Batman reúne uma nova equipe formada por heróis míticos como o Monstro do Pântano, o Demônio e Constantine.
+", 
+trailer: "https://www.youtube.com/watch?v=w5xyNvjcqCg
+" 
+} 
+];
+export const movies = [
+{
+title: ":festa da salsicha",
+genre: "Animção",
+ageRating: 16,
+synopsis: "Frank, a salsicha, e seus amigos tentam evitar serem comprados e comidos por humanos em um supermercado.
+",
+trailer: "https://www.youtube.com/watch?v=wRkw7BxKBFo
+"
+},
+{
+title: " Como cães e gatos
+",
+genre: "Suspense",
+ageRating: Livre,
+synopsis: "Cães e gatos têm travado uma guerra secreta desde o começo dos tempos. A vitória dos cães fica cada vez mais próxima quando o Professor Brody inventa uma fórmula capaz de curar a alergia dos humanos a cachorros. Para impedi-la, o líder dos gatos, Sr. Tinkles, decide roubar a vacina, mas primeiro precisa enfrentar o Agente Lou, um cão corajoso que, graças a um erro no sistema, está encarregado de proteger a família Brody.
+",
+trailer: "https://youtu.be/n-erwqEDoiQ?si=kgmOsatcCM-LsaR6
+"
+},
+{
+title: "incontrolável 
+",
+genre: "Suspense",
+ageRating: 10,
+synopsis: "Uma grande locomotiva, sem tripulação, está fora de controle, e um descarrilamento não parece ser o maior problema. O trem está carregado de produtos químicos tóxicos e um acidente poderia destruir uma cidade, causando um desastre ambiental. A única esperança de parar o trem com segurança está nas mãos de um maquinista veterano e um jovem condutor, que arriscam suas vidas para salvar aqueles no caminho do trem desgovernado.
+",
+trailer: "https://youtu.be/mlSS-trk_hU?si=xhwrEsDh3-OIOOU2
+"
+},
+{
+title: "sinais",
+genre: "suspense",
+ageRating: 12,
+synopsis: "No condado de Bucks, Pensilvânia, vive o viúvo Graham Hess e seus dois filhos, Morgan e Bo. Ele costumava ser o pastor da região, mas se recusa a ser chamado assim desde que sua mulher, Colleen, morreu atropelada por Ray Reddy, um de seus vizinhos. Os Hess ficam intrigados com o aparecimento de misteriosos círculos em sua plantação sem que haja o menor vestígio de quem os fez ou por qual motivo teriam sido feitos.
+",
+trailer: "https://youtu.be/jFdf5NAkQFk?si=fZhtboZVhNLVZnqG
+"
+},
+{
+title: "sexta-feira 13 (1985)
+",
+genre: "suspense",
+ageRating: 14,
+synopsis: "A história de um antigo homicídio em Crystal Lake não impede que alguns jovens montem um acampamento de verão no bosque. Moradores supersticiosos advertem sobre o ocorrido, mas os instrutores Jack, Alice, Bill, Marcie e Ned prestam pouca atenção aos mais velhos e acabam sendo perseguidos por um assassino brutal.
+",
+trailer: "https://youtu.be/dGT2-aWLHDw?si=YmuSDad4dXzIVYi8
+"
+},
+{
+title: "jason x ",
+genre: "suspense",
+ageRating: 16,
+synopsis: "O ano é 2455. O Lugar é a Terra Velha. Uma vez a joia azul cintilante da galáxia, a Terra Velha é agora um planeta contaminado abandonado por séculos. No entanto, os seres humanos voltaram ao lugar mortal de onde uma vez fugiram. Não para viver, mas para pesquisar os artefatos antigos enferrujados das civilizações passadas, que causaram esse desastre ambiental. E pouco faz o grupo de desembarque mais recente de ousados e jovens exploradores perceber o destino que os espera.
+",
+trailer: "https://youtu.be/XuMNAt7xQa8?si=Oq-7Jmv8h2lBCybg
+"
+},
+{
+title: "o último garoto da terra ",
+
+genre: "terror",
+ageRating: livre,
+synopsis: " Em um futuro distante, um garoto enigmático se torna a figura central na busca por uma nova esperança. Quem é esse garoto? Por que todo mundo está procurando por isso? Às vezes é melhor não saber certas respostas
+",
+trailer: "https://youtu.be/VSvDlKqKRsc?si=L19QtmRvyH7PIq4H
+"
+},
+{
+title: "Five nights at freddy’s ",
+genre: "Terror",
+ageRating: 10,
+synopsis: "No Freddy Fazbear's Pizza, robôs animados fazem a festa das crianças durante o dia. Mas, quando chega a noite, eles se transformam em assassinos psicopatas. 
+",
+trailer: "https://youtu.be/h2lVX71L_3A?si=0mincRrcCtVyJ0Mc
+"
+},
+{
+title: "",
+genre: "",
+ageRating: 16,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 13,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 16,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 18,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 18,
+synopsis: "",
+trailer: ""
+},
+
+{
+title: "",
+genre: "",
+ageRating: 13,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 16,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 13,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 18,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 13,
+synopsis: "",
+trailer: ""
+}
+];
+export const movies = [
+{
+title: "",
+genre: "",
+ageRating: 10,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 10,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 10,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 13,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 13,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 16,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+
+genre: "",
+ageRating: 16,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 18,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 16,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 13,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 16,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 18,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 18,
+synopsis: "",
+trailer: ""
+},
+
+{
+title: "",
+genre: "",
+ageRating: 13,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 16,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 13,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 18,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 13,
+synopsis: "",
+trailer: ""
+}
+];
+
